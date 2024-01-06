81101ea45889428c8f79374f74505a4df9767269
GorenzelgProjects <s214649@student.dtu.dk>
2024-01-06 16:13:00 +0100

    Update train_model.py
    Implemented v.1 of Hydra implementaion for hyperparametersettings in train loop.

--- MLOpsProject/train_model.py
+++ MLOpsProject/train_model.py
@@ -4,36 +4,47 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 from models.model import LinearNeuralNetwork
 from data.dummy_data_loader import dummy_data
+from config import CSGOConfig
+import hydra
+from hydra.core.config_store import ConfigStore
 
+cs = ConfigStore.instance()
+cs.store(name="csgo_config", node=CSGOConfig)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 net = LinearNeuralNetwork().to(device)
 
+
+
 #Hyperparameters
-lr = 0.001
-batch_size = 10
-epochs = 5
-model_save_path = f"models/trained_model.pt"
+#lr = 0.001
+#batch_size = 10
+#epochs = 5
+#model_save_path = f"models/trained_model.pt"
 
 # Loss function and optimizer
-criterion = nn.BCELoss()  # Binary Cross-Entropy Loss for binary classification
-optimizer = optim.Adam(net.parameters(), lr=lr)
+#criterion = nn.BCELoss()  # Binary Cross-Entropy Loss for binary classification
+#optimizer = optim.Adam(net.parameters(), lr=lr)
 
 
-# Training loop
-def train(model: torch.nn.Module,
-        dataloader: torch.utils.data.DataLoader, 
-        criterion: torch.nn.Module, 
-        optimizer: torch.optim.Optimizer, 
-        epochs: int
-        ) -> None:
+#Hydra decrator to read config file
+@hydra.main(config_path="conf", config_name="config")
+def train(cfg: CSGOConfig) -> None:
+    print(cfg.params)
     """Train a model."""
-    print(f"Training with learning rate {lr} and epochs {epochs}")
-    model.train()
-    for epoch in range(epochs):
+    print(f"Training with learning rate {cfg.params.lr} and epochs {cfg.params.epochs}")
+    model = net.to(device)
+    print(cfg.optim.optimizer)
+    #optimizer = cfg.optim.optimizer(model.parameters(), lr=cfg.params.lr)
+    #criterion = cfg.loss.criterion()
+    optimizer = optim.Adam(model.parameters(), lr=cfg.params.lr)
+    criterion = nn.BCELoss()
+    #return
+    _,_,_,_,train_loader, _ = dummy_data(batch_size=cfg.params.batch_size)
+    for epoch in range(cfg.params.epochs):
         running_loss = 0.0   
-        for data in dataloader:
+        for data in train_loader:
             inputs, labels = data[0].to(device), data[1].to(device)
             optimizer.zero_grad()
             outputs = model(inputs)
@@ -43,8 +54,9 @@ def train(model: torch.nn.Module,
             running_loss += loss.item()
         print(f'Epoch {epoch + 1}, Loss: {running_loss / len(train_loader)}')
     print('Finished Training')
-    torch.save(model, model_save_path)
+    torch.save(model, cfg.files.model_save_path)
 
 if __name__ == "__main__":
-    _,_,_,_,train_loader, _ = dummy_data(batch_size=batch_size)
-    train(net, train_loader, criterion, optimizer, epochs=epochs)
+    #_,_,_,_,train_loader, _ = dummy_data(batch_size=batch_size)
+    #train(net, train_loader, criterion, optimizer)
+    train()
