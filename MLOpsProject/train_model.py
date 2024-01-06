bdece0cbd4f5a3840a0b8fab938226736765b706
GorenzelgProjects <s214649@student.dtu.dk>
2024-01-06 16:37:15 +0100

    Update train_model.py
    Updated the train to use the CSGO dataset from Kaggle.

--- MLOpsProject/train_model.py
+++ MLOpsProject/train_model.py
@@ -4,6 +4,8 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 from models.model import LinearNeuralNetwork
 from data.dummy_data_loader import dummy_data
+from data.cs_data_loader import cs_data
+from os.path import dirname as up
 from config import CSGOConfig
 import hydra
 from hydra.core.config_store import ConfigStore
@@ -36,19 +38,33 @@ def train(cfg: CSGOConfig) -> None:
     print(f"Training with learning rate {cfg.params.lr} and epochs {cfg.params.epochs}")
     model = net.to(device)
     print(cfg.optim.optimizer)
+
+    """This is getting removed in future versions:start"""
     #optimizer = cfg.optim.optimizer(model.parameters(), lr=cfg.params.lr)
     #criterion = cfg.loss.criterion()
     optimizer = optim.Adam(model.parameters(), lr=cfg.params.lr)
     criterion = nn.BCELoss()
     #return
-    _,_,_,_,train_loader, _ = dummy_data(batch_size=cfg.params.batch_size)
+    one_up = up(up(__file__))
+    # replace '\\' with '/' for Windows
+    one_up = one_up.replace('\\', '/')
+    # Join the paths to the csv files:
+    file_path = one_up + "/data/processed/csgo_converted.csv"
+    #_,_,_,_,train_loader, _ = dummy_data(batch_size=batch_size)
+    train_loader, _, _ = cs_data(file_path=file_path)
+
+    #_,_,_,_,train_loader, _ = dummy_data(batch_size=cfg.params.batch_size)
+
+    """This is getting removed in future versions:end"""
+
+
     for epoch in range(cfg.params.epochs):
         running_loss = 0.0   
         for data in train_loader:
             inputs, labels = data[0].to(device), data[1].to(device)
             optimizer.zero_grad()
             outputs = model(inputs)
-            loss = criterion(outputs, labels)
+            loss = criterion(outputs, labels.unsqueeze(1))
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
@@ -59,4 +75,8 @@ def train(cfg: CSGOConfig) -> None:
 if __name__ == "__main__":
     #_,_,_,_,train_loader, _ = dummy_data(batch_size=batch_size)
     #train(net, train_loader, criterion, optimizer)
-    train()
+    #train()
+    
+
+
+    train() 
