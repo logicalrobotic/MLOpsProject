185b1e5f6f3cd3e159b8c89591cd468d71416911
GorenzelgProjects <s214649@student.dtu.dk>
2024-01-06 14:21:04 +0100

    Update predict_model.py
    Created a bare-minimum test-loop for the initial model.

--- MLOpsProject/predict_model.py
+++ MLOpsProject/predict_model.py
@@ -1,17 +1,30 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from models.model import LinearNeuralNetwork
+from data.dummy_data_loader import dummy_data
 
 def predict(
     model: torch.nn.Module,
     dataloader: torch.utils.data.DataLoader
 ) -> None:
-    """Run prediction for a given model and dataloader.
-    
-    Args:
-        model: model to use for prediction
-        dataloader: dataloader with batches
-    
-    Returns
-        Tensor of shape [N, d] where N is the number of samples and d is the output dimension of the model
+    total = 0
+    correct = 0
+    model.eval()
+    with torch.no_grad():
+        for data in test_loader:
+            inputs, labels = data[0].to(device), data[1].to(device)
+            outputs = net(inputs)
+            predicted = (outputs > 0.5).float()
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print(f'Accuracy: {100 * correct / total}%')
+    return 
 
-    """
-    return torch.cat([model(batch) for batch in dataloader], 0)
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    _,_,_,_,_, test_loader = dummy_data()
+    net = LinearNeuralNetwork().to(device)
+    #net.load_state_dict(torch.load('models/model.pt'))
+    predict(net, test_loader)
