1e1539ed9a2709fa32b167f12f7eb019f8c66890
GorenzelgProjects <s214649@student.dtu.dk>
2024-01-11 14:27:46 +0100

    Update train_model.py
    Changed criterion and new dataloading method. Furthermore added some print statements for debugging.

--- MLOpsProject/train_model.py
+++ MLOpsProject/train_model.py
@@ -44,8 +44,7 @@ def train(cfg: CSGOConfig) -> None:
     wandb.config.update({"lr": cfg.params.lr, "epochs": cfg.params.epochs})
 
     wandb.init(project="csgo")
-    """Train a model."""
-    print(f"Training with learning rate {cfg.params.lr} and {cfg.params.epochs} epochs")
+    
     model = net.to(device)
     wandb.watch(model,log_freq=1000)
     #print(cfg.optimizer)
@@ -54,15 +53,17 @@ def train(cfg: CSGOConfig) -> None:
     #optimizer = cfg.optim.optimizer(model.parameters(), lr=cfg.params.lr)
     #criterion = cfg.loss.criterion()
     #optimizer = optim.Adam(model.parameters(), lr=cfg.params.lr)
-    criterion = nn.BCELoss()
+    criterion = nn.CrossEntropyLoss()
     #return
     one_up = up(up(__file__))
     # replace '\\' with '/' for Windows
     one_up = one_up.replace('\\', '/')
     # Join the paths to the csv files:
-    file_path = one_up + "/data/processed/csgo_converted.csv"
-    #_,_,_,_,train_loader, _ = dummy_data(batch_size=batch_size)
-    train_loader, _, _ = cs_data(file_path=file_path)
+    file_path = one_up + "/data/processed/train_loader.pth"
+    print("Loading data from: ", file_path)
+    train_loader = torch.load(file_path)
+    
+    #train_loader, _, _ = cs_data(file_path=file_path)
 
     #_,_,_,_,train_loader, _ = dummy_data(batch_size=cfg.params.batch_size)
 
@@ -70,14 +71,19 @@ def train(cfg: CSGOConfig) -> None:
     #optimizer = cfg.optimizer._target_(model.parameters(), lr=cfg.params.lr)
     #print(instantiate(cfg.optimizer, params=model.parameters()))
     optimizer = instantiate(cfg.optimizer, params=model.parameters())
-  
+    """Train a model."""
+    print(f"Training with learning rate {cfg.params.lr} and {cfg.params.epochs} epochs")
     for epoch in range(cfg.params.epochs):
         running_loss = 0.0   
         for data in train_loader:
             inputs, labels = data[0].to(device), data[1].to(device)
             optimizer.zero_grad()
             outputs = model(inputs)
-            loss = criterion(outputs, labels.unsqueeze(1))
+            print("outputs: ",outputs,outputs.shape, outputs.dtype)
+            print("labels: ",labels.long(),labels.long().shape, labels.long().dtype)
+                #print("shapes: ",outputs.shape, labels.shape, labels.unsqueeze(1).shape)
+                #print("type: ",outputs.dtype,labels.dtype, labels.unsqueeze(1).dtype)
+            loss = criterion(outputs, labels.long())
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
