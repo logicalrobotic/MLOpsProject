613f45f842b3724bf3ba68ac490270035f541200
GorenzelgProjects <s214649@student.dtu.dk>
2024-01-11 23:36:24 +0100

    Update predict_model.py
    minor changes and clean-up of the prediction loop and test-loop.

--- MLOpsProject/predict_model.py
+++ MLOpsProject/predict_model.py
@@ -2,48 +2,43 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
-from models.model import LinearNeuralNetwork
+from models.model import NeuralNetwork
 from data.dummy_data_loader import dummy_data
 import numpy as np
 from data.cs_data_loader import cs_data
 from os.path import dirname as up
+from data.clean_data import *
+from torchvision import datasets, transforms
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-net = LinearNeuralNetwork().to(device)
+net = NeuralNetwork().to(device)
 
 def predict(
     model: torch.nn.Module,
     dataloader: torch.utils.data.DataLoader
 ) -> None:
     total = 0
-    correct = []
+    correct = 0
     model = torch.load(model)
     model.eval()
     with torch.no_grad():
-        for data in dataloader:
-            inputs, labels = data[0].to(device), data[1].to(device)
-            outputs = net(inputs)
+        for images, labels in dataloader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images.float())
             #torch max of outputs
             _, predicted = torch.max(outputs.data, 1)
-            correct.append((predicted == labels).sum().item())
-            #print(correct)
-    print(f'Accuracy: {np.mean(correct)}%')
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    test_accuracy = 100*(correct / total)
+    print(f'Test Accuracy: {test_accuracy:.3f}%')
     return 
 
+
 if __name__ == "__main__":
-    #_,_,_,_,_, test_loader = dummy_data()
     one_up = up(up(__file__))
     # replace '\\' with '/' for Windows
     one_up = one_up.replace('\\', '/')
-    # Join the paths to the csv files:
-    file_path = one_up + "/data/processed/train_loader.pth"
-    print("Loading data from: ", file_path)
-    train_loader = torch.load(file_path)
-    file_path = one_up + "/data/processed/test_loader.pth"
-    print("Loading data from: ", file_path)
-    test_loader = torch.load(file_path)
-    print("Data loaded successfully!")
-    print("Predicting on train data")
-    predict("trained_model.pt",train_loader)
-    print("Predicting on test data")
+    test_path = one_up + "/data/processed/test_loader.pth"
+    print("Loading data from: ", test_path)
+    test_loader = torch.load(test_path)
     predict("trained_model.pt",test_loader)
