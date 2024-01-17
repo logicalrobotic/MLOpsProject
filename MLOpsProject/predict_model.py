5900d3e88683da4be13f4e07eaccb25f2b16825c
GorenzelgProjects <s214649@student.dtu.dk>
2024-01-17 15:06:38 +0100

    Update predict_model.py
    Updated imports.
    Added cleaner setup.
    Added updating model to newest.
    Added verbose for print statements.

--- MLOpsProject/predict_model.py
+++ MLOpsProject/predict_model.py
@@ -1,44 +1,59 @@
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
-from models.model import NeuralNetwork
-from data.dummy_data_loader import dummy_data
-import numpy as np
-from data.cs_data_loader import cs_data
-from os.path import dirname as up
 from data.clean_data import *
-from torchvision import datasets, transforms
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-net = NeuralNetwork().to(device)
+from helper import *
 
 def predict(
     model: torch.nn.Module,
-    dataloader: torch.utils.data.DataLoader
-) -> None:
+    dataloader: torch.utils.data.DataLoader,
+    verbose: bool = False,
+    logging: bool = False,
+) -> list:
+    
+    """Main prediction routine.
+
+    Modes: normal, verbose,logging. Change mode in function input.
+        -> normal: no logging or debugging. Defaults to this mode if no mode is specified.
+        -> verbose: prints the device used and the test accuracy
+        -> logging: logs the loss and accuracy to wandb
+
+    Returns: List of softmax of the model on the test set
+
+    """
+
+    #Move the newest model to the models folder
+    source_folder = "./outputs/"
+    destination_folder = "./MLOpsProject/models"
+    newest_model = find_newest_pt_model(source_folder)
+    move_model_to_folder(newest_model, destination_folder,verbose = verbose)
+
+    # Set device to GPU if available
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if verbose:
+        print("Using device: ", device)
+    
+    """Prediction loop of model"""
+
     total = 0
     correct = 0
+    all_predictions = []
     model = torch.load(model)
     model.eval()
     with torch.no_grad():
         for images, labels in dataloader:
             images, labels = images.to(device), labels.to(device)
             outputs = model(images.float())
-            #torch max of outputs
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
+            all_predictions.append(outputs)
     test_accuracy = 100*(correct / total)
-    print(f'Test Accuracy: {test_accuracy:.3f}%')
-    return 
+    if verbose:
+        print(f'Test Accuracy: {test_accuracy:.3f}%')
+    return all_predictions
 
 
 if __name__ == "__main__":
-    one_up = up(up(__file__))
-    # replace '\\' with '/' for Windows
-    one_up = one_up.replace('\\', '/')
-    test_path = one_up + "/data/processed/test_loader.pth"
+    test_path = "MLOpsProject/data/processed/test_loader.pth"
     print("Loading data from: ", test_path)
     test_loader = torch.load(test_path)
-    predict("trained_model.pt",test_loader)
+    predict("MLOpsProject/models/trained_model.pt",test_loader,verbose=True)
