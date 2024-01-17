a09ad14b364ba08eb30b24cf722a2b5071a2a85b
GorenzelgProjects <s214649@student.dtu.dk>
2024-01-17 15:05:24 +0100

    Update helper.py
    Updated helper functions to copy model instead of moving it.
    Added verbose parameter to some print statements.

--- MLOpsProject/helper.py
+++ MLOpsProject/helper.py
@@ -14,7 +14,7 @@ def find_newest_pt_model(src_folder):
 
     return newest_model
 
-def move_model_to_folder(src_model, dest_folder):
+def move_model_to_folder(src_model, dest_folder,verbose=False):
     if src_model is not None:
         # Create destination folder if it doesn't exist
         os.makedirs(dest_folder, exist_ok=True)
@@ -24,8 +24,9 @@ def move_model_to_folder(src_model, dest_folder):
         # Check if the source file is newer than the destination file
         if not dest_model.exists() or src_model.stat().st_mtime > dest_model.stat().st_mtime:
             # Move the model file to the destination folder
-            shutil.move(str(src_model), dest_model)
-            print(f"Moved {src_model.name} to {dest_folder}")
+            shutil.copy(str(src_model), dest_model)
+            if verbose:
+                print(f"Moved {src_model.name} to {dest_folder}")
         else:
             print(f"{src_model.name} is not newer than the existing file in {dest_folder}. Keeping the original file.")
     else:
