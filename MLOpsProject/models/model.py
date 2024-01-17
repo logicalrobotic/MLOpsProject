4463a4da62a4edd54546f42d89143e0da6674578
GorenzelgProjects <s214649@student.dtu.dk>
2024-01-18 00:36:14 +0100

    Update model.py
    Added complexity to the network

--- MLOpsProject/models/model.py
+++ MLOpsProject/models/model.py
@@ -6,22 +6,37 @@ class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
         self.fc1 = nn.Linear(104, 256)
+        #nn.init.xavier_uniform_(self.fc1.weight)
+        #nn.init.zeros_(self.fc1.bias)
         self.relu1 = nn.ReLU()
+        self.dropout1 = nn.Dropout(0.2)
+        #self.bn1 = nn.BatchNorm1d(256)
         self.fc2 = nn.Linear(256, 128)
         self.relu2 = nn.ReLU()
+        self.dropout2 = nn.Dropout(0.2)
+        #self.bn2 = nn.BatchNorm1d(128)
         self.fc3 = nn.Linear(128, 32)
         self.relu3 = nn.ReLU()
+        self.dropout3 = nn.Dropout(0.2)
+        #self.bn3 = nn.BatchNorm1d(32)
         self.fc4 = nn.Linear(32, 2)
+
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = x.view(x.size(0), -1)  # Flatten the input tensor
         x = self.fc1(x)
         x = self.relu1(x)
+        x = self.dropout1(x)
+        #x = self.bn1(x)
         x = self.fc2(x)
         x = self.relu2(x)
+        x = self.dropout2(x)
+        #x = self.bn2(x)
         x = self.fc3(x)
         x = self.relu3(x)
+        x = self.dropout3(x)
+        #x = self.bn3(x)
         x = self.fc4(x)
         x = self.softmax(x)
         return x
